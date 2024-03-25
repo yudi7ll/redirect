cffcfb7f7397ed39bff1b52d8d3978a8211a2d94
Yudi <madewahyudi22@gmail.com>
2024-03-25 12:01:22 +0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 3000; 
+
+app.get('/', (req, res) => {
+  res.redirect(process.env.TARGET_URL);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
